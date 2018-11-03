a919d5754b8935d2e56485c65b45b86e0db2da68
Kartik Malik <kartikmalik91@gmail.com>
2018-11-03 16:15:59 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import FileUploader from '../components/fileUploader';
 
 const IndexPage = () => (
   <Layout>
-    <Link to="/page-2/">Go to page 2</Link>
     <FileUploader />
   </Layout>
 )
