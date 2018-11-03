02a45c00331a78b01b8519c232acd2f96745ccaf
badgamerbad <badgamerbad@gmail.com>
2018-11-04 01:27:26 +0530

    fixed body background stretching and added css for header

--- src/components/header.js
+++ src/components/header.js
@@ -5,15 +5,16 @@ const Header = ({ siteTitle }) => (
   <div
     style={{
       marginBottom: '1.45rem',
+      background: 'white'
     }}
   >
     <div
       style={{
         margin: '0 auto',
-        padding: '15px',
+        padding: '15px'
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 style={{ margin: 0, background: '-webkit-linear-gradient(#1f9389, #a5d9a1)', '-webkit-background-clip': 'text', '-webkit-text-fill-color': 'transparent', 'font-size': '4em', padding: '0.2em 2.5em 0.4em' }}>
         <Link
           to="/"
           style={{
