d45a17db68058e1dd721a969b9dd490508c0050f
Kartik Malik <kartikmalik91@gmail.com>
2018-11-03 21:51:07 +0530

    Revert "Refactoring clarifai code"

--- src/utils/clarifai.js
+++
@@ -1,8 +0,0 @@
-import Clarifai from 'clarifai'
-
-const app = new Clarifai.App({
-  apiKey: process.env.CLARIFAI_API_KEY,
-})
-
-export const getIngredients = url =>
-  app.models.predict('bd367be194cf45149e75f01d59f77ba7', url)
