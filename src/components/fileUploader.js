d358c49b454ff9e0f4cb034a65936ea85069bc70
Vipul Dessai <badgamerbad@gmail.com>
2019-11-27 22:23:52 +0530

    added axios for PUT files

--- src/components/fileUploader.js
+++ src/components/fileUploader.js
@@ -9,6 +9,8 @@ import Calorie from '../assets/icons/calorie.svg'
 import Protein from '../assets/icons/protein.svg'
 import C from '../assets/icons/c.svg'
 
+import axios from 'axios'
+
 //TODO: seperate clarifai logic into utils
 
 export default class FileUploader extends Component {
@@ -33,46 +35,29 @@ export default class FileUploader extends Component {
     })
   }
   handleSubmit(event) {
+    event.preventDefault()
+
     let { onLoadStateChange } = this.props
     onLoadStateChange('25%') //start the loader
-    event.preventDefault()
     const file = this.fileInput.files[0]
     this.setState({
       uploadedImage: null,
       ingredients: [],
     })
-    let uploadTask = storage
-      .ref()
-      .child(file.name)
-      .put(file)
 
-    uploadTask.on(
-      'state_changed',
-      snapshot => {
-        // Handle progress of upload
-        let progress = Math.round(
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        )
-        this.setState({ progress })
-      },
-      error => {
-        // Handle unsuccessful upload
-      },
-      () => {
-        // Handle successful upload
-        this.setState({
-          progress: 100,
-        })
-        uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
-          console.log(`Your uploaded image is now available at ${downloadURL}`)
-          this.setState({
-            uploadedImage: downloadURL,
-          })
-          this.setImage(downloadURL)
-          onLoadStateChange('75%')
-        })
-      }
-    )
+    let signedUrlfromGcp = "https://storage.googleapis.com/ingredofit.appspot.com/IMG-20161021-WA0017.jpg?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=ingredo-fit-db-account%40ingredofit.iam.gserviceaccount.com%2F20191127%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20191127T163502Z&X-Goog-Expires=28800&X-Goog-SignedHeaders=host&X-Goog-Signature=276dac5e4aae751bf988c1f2478981da47417a713f1502f6592823cae9c1fa126e25f2bac63bc1e910357ce598191d957a0a8564b62f087b8f52513a89dbabeb2e9a69734cff26aeb9b49847095c6ec1d220f8606613fc1fb22a77bc19b7c75d933e5243b7fa4f8c513c82a4ecd2784541831a0fb65c8b89dbb5ebd870fafda628aafd24f48d2073cacfbed83c8c8772ba325dbd0f0d94f871241eb14f45fd183f27726104fe8f0eda139d4c09935c79c9bdadd234068f604ed413cc327c3952e70c218ab4f0b2c0a0b65252a0ae94f321b4f643ec340eb474fe496af8648d57d668a3a0f984c8bdcfa3d1b6ab63d099b76a28a08034c54483b78412d0a0db61";
+
+    axios.put(signedUrlfromGcp, file, {
+        headers: {
+          'Content-Type': file.type
+        }
+    })
+    .then(res =>{
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    });
   }
   handleSelect() {
     this.fileInput.click()
