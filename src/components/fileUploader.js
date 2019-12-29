8fef6fac75824921530079095e1617be3c13cfee
Vipul Dessai <badgamerbad@gmail.com>
2019-12-29 22:41:05 +0530

    securing clarifai api

--- src/components/fileUploader.js
+++ src/components/fileUploader.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-import Clarifai from 'clarifai'
 import classNames from 'classnames'
-import { FOOD_DATA } from '../utils/data'
 import './fileUploader.scss'
 import Food from '../assets/icons/food.svg'
 import Calorie from '../assets/icons/calorie.svg'
@@ -9,8 +7,6 @@ import Protein from '../assets/icons/protein.svg'
 import C from '../assets/icons/c.svg'
 
 import axios from 'axios'
-
-// TODO: seperate clarifai logic into utils
 
 export default class FileUploader extends Component {
   constructor(props) {
@@ -23,9 +19,6 @@ export default class FileUploader extends Component {
       fileName: '',
     }
     this.handleSubmit = this.handleSubmit.bind(this)
-    this.app = new Clarifai.App({
-      apiKey: process.env.CLARIFAI_API_KEY,
-    })
   }
   handleChange(event) {
     this.setState({
@@ -74,7 +67,8 @@ export default class FileUploader extends Component {
       // fetch the url of the uploaded file and
       // the details of the Clarifai ingredients API
       else {
-
+        // after fetching the clarifai ingredients from aws
+        // lambda api
       }
     }
   }
@@ -84,31 +78,10 @@ export default class FileUploader extends Component {
   setImage(url) {
     let { onLoadStateChange } = this.props
 
-    this.app.models.predict('bd367be194cf45149e75f01d59f77ba7', url).then(
-      response => {
-        console.log(response)
-        // do something with response
-        let concepts = response['outputs'][0]['data']['concepts']
-
-        let ingredients = concepts.map(concept => {
-          let ingredient = FOOD_DATA.find(food => food.name === concept.name)
-          return {
-            ...concept,
-            ...ingredient,
-          }
-        })
-        // console.log(concepts)
-        this.setState({
-          ingredients,
-        })
-
-        console.log('concepts', this.state.ingredients)
-        onLoadStateChange('100%')
-      },
-      err => {
-        // there was an error
-      }
-    )
+    onLoadStateChange('100%')
+    this.setState({
+      uploadedImage: url,
+    })
   }
 
   render() {
